ea74f592765278b8ab31542b45fbc55500cb73fc
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-06-30 21:35:44 -0300

    Criar ConstantesTela.java

---
+++ app/src/main/java/reutilizaveis/TelaLoading1/configuracao/ConstantesTela.java
@@ -0,0 +1,11 @@
+package reutilizaveis.telaloading1.configuracao;
+
+public class ConstantesTela {
+    // Tempos padrão de animações em milissegundos
+    public static final long DURACAO_FADE_MS = 400;
+    public static final long DURACAO_BRILHO_MS = 1500;
+    public static final long DURACAO_PROGRESSO_MS = 300;
+    
+    // Configurações padrão do loop de dicas
+    public static final int INTERVALO_DICAS_PADRAO_MS = 4000;
+}
