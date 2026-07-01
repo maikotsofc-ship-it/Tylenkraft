9b6e56d79d5a3e7fac9662b60aa41239e79b9e16
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-06-30 21:37:07 -0300

    Criar dicas/Dica.java

---
+++ app/src/main/java/reutilizaveis/TelaLoading1/dicas/Dica.java
@@ -0,0 +1,13 @@
+package reutilizaveis.telaloading1.dicas;
+
+public class Dica {
+    private final String texto;
+
+    public Dica(String texto) {
+        this.texto = texto;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+}
